b10e594c79f71a30e8163278f60f5904c5414479
MutisyaVuta <nicholas.mutisya77@gmail.com>
2024-03-21 23:46:58 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun (name= 'roller-skate'){
+    return (`This Saturday, I want to ${name}!`);
+    
+}
+saturdayFun ("bathe my dog");
+
+const mondayWork = function(activity='go to the office'){
+    return (`This Monday, I will ${activity}.`)
+
+}
+mondayWork("work from home.");
+
+let wrapAdjective = function (){
+    
+    let emphatic = result("a dedicated programmer")
+    return function(name ="special"){
+        return (`You are ${name}!`)
+
+    }
+
+}
+
+
+
+
